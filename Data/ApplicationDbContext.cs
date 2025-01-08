7b86f087b9ca317581495815ac094a29aa447435
Shailo Douglas <sdshailo88@gmail.com>
2025-01-08 14:11:48 +0100

    Added seed data for Animal and updated EF migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,10 +17,34 @@ namespace Dierentuin.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // Hier kun je eventueel seeding-data toevoegen, bijvoorbeeld voor de categorieën
+            // Seeding data for categories
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Mammals" },
                 new Category { Id = 2, Name = "Reptiles" }
+            );
+
+            // Seeding data for animals
+            modelBuilder.Entity<Animal>().HasData(
+                new Animal
+                {
+                    Id = 1,
+                    Name = "Lion",
+                    Species = "Panthera leo",
+                    CategoryId = 1,
+                    Size = SizeEnum.Large,
+                    DietaryClass = DietaryClassEnum.Carnivore,
+                    ActivityPattern = ActivityPatternEnum.Diurnal
+                },
+                new Animal
+                {
+                    Id = 2,
+                    Name = "Python",
+                    Species = "Python regius",
+                    CategoryId = 2,
+                    Size = SizeEnum.Medium,
+                    DietaryClass = DietaryClassEnum.Carnivore,
+                    ActivityPattern = ActivityPatternEnum.Nocturnal
+                }
             );
         }
     }
---
+++ Migrations/20250108130927_SeedAnimalData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Dierentuin.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAnimalData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "ActivityPattern", "CategoryId", "DietaryClass", "Name", "Size", "Species" },
+                values: new object[,]
+                {
+                    { 1, 0, 1, 0, "Lion", 4, "Panthera leo" },
+                    { 2, 1, 2, 0, "Python", 3, "Python regius" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
