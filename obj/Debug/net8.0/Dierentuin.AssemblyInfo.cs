eda7e5ed95642773d1fd688a206c4486bf587a9b
Shailo Douglas <sdshailo88@gmail.com>
2025-01-09 17:29:11 +0100

    added row in animals table

--- obj/Debug/net8.0/Dierentuin.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dierentuin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dierentuin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358e9ae863dd01a0b78cb77339c74dbacee87294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235cf60413527045ddbc2c2508e6565d28610ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dierentuin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dierentuin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
