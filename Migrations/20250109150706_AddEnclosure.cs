e085d11933c69bacdc2b30fe38e92ed55c60a2b3
Hcaliskan <52010720+Hcaliskan40@users.noreply.github.com>
2025-01-09 16:25:26 +0100

    changes

---
+++ Migrations/20250109150706_AddEnclosure.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dierentuin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnclosure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnclosureId",
+                table: "Animals",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Enclosures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Climate = table.Column<int>(type: "INTEGER", nullable: false),
+                    HabitatType = table.Column<int>(type: "INTEGER", nullable: false),
+                    SecurityLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    Size = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enclosures", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EnclosureId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "EnclosureId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_EnclosureId",
+                table: "Animals",
+                column: "EnclosureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animals_Enclosures_EnclosureId",
+                table: "Animals",
+                column: "EnclosureId",
+                principalTable: "Enclosures",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animals_Enclosures_EnclosureId",
+                table: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Enclosures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Animals_EnclosureId",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "EnclosureId",
+                table: "Animals");
+        }
+    }
+}
