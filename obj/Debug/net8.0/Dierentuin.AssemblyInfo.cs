720f4d69e41b2ec6559a244b706affd7426713fc
Shailo Douglas <sdshailo88@gmail.com>
2025-01-10 01:37:48 +0100

    .

--- obj/Debug/net8.0/Dierentuin.AssemblyInfo.cs
+++ obj/Debug/net8.0/Dierentuin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dierentuin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c95c71a4cb9eb3f7685c55dd87028db5ed030f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358e9ae863dd01a0b78cb77339c74dbacee87294")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dierentuin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dierentuin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
